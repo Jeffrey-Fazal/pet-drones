44257f9cb71ba015831a669979f91c8b00bc7b7e
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-21 10:08:12 +1100

    hail marry navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,26 @@
-import React from 'react'
-import Box, { BoxProps } from '@mui/material/Box';
+import React from 'react';
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 
-
 function NavItem(props) {
-    const { children, ...other } = props;
+    const { children } = props;
     return (
-      <Box sx={{ p: 1, m: 1 }}>
-        <IconButton {...other}>{children}</IconButton>
-      </Box>
+        <div style={{ padding: "8px" }}>
+            <IconButton>{children}</IconButton>
+        </div>
     );
-  }
-  
-  export default function NavBar() {
+}
+
+export default function NavBar() {
     return (
-      
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          p: 1,
-          bgcolor: "primary.main"
-        }}
-      >
-        <NavItem>
-          <MenuIcon sx={{ color: "common.white" }} />
-        </NavItem>
-        <NavItem>
-          <SearchIcon sx={{ color: "common.white" }} />
-        </NavItem>
-      </Box>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "primary.main" }}>
+            <NavItem>
+                <MenuIcon style={{ color: "white" }} />
+            </NavItem>
+            <NavItem>
+                <SearchIcon style={{ color: "white" }} />
+            </NavItem>
+        </div>
     );
-  }
-  
+}
